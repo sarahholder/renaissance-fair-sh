fa9d92f571bc666906cb076af42946b0c2a1f3e8
Kayla Melton <kannmelton2@gmail.com>
2020-04-16 10:32:22 -0500

    printing show cards to page

--- src/javascripts/components/shows/shows.js
+++ src/javascripts/components/shows/shows.js
@@ -1,12 +1,19 @@
 import showData from '../../helpers/data/showData';
 
+import showCards from '../showCards/showCards';
+
 import utils from '../../helpers/utils';
 
 const buildAllShows = () => {
   showData.getShows()
     .then((shows) => {
-      const domString = '<h2>Shows</h2>';
-      console.error(shows);
+      let domString = '<h2 class="mt-3 ml-3">Shows</h2>';
+      domString += '<h3 class="ml-3">Fun and interactive shows for all ages</h3>';
+      domString += '<div class="container-fluid d-flex flex-wrap">';
+      shows.forEach((show) => {
+        domString += showCards.buildShowCards(show);
+      });
+      domString += '</div>';
       utils.printToDom('shows', domString);
     })
     .catch((err) => console.error('get shows failed', err));
