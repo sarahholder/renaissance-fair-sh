da56dd9347b19f867af4e7704b18725abee430db
Beth <bethhnielsen56@gmail.com>
2020-04-15 22:37:52 -0500

    wip

--- src/javascripts/components/foods/food.js
+++ src/javascripts/components/foods/food.js
@@ -1,12 +1,19 @@
-// import utils from '../../helpers/utils';
+import utils from '../../helpers/utils';
 import foodData from '../../helpers/data/foodData';
+import foodComponent from './foodComponent';
+import './food.scss';
 
 const buildAllFoods = () => {
+  let domString = '';
   foodData.getFoods()
     .then((foods) => {
-      foods.forEach((foodInformation) => {
-        console.error(foodInformation.type);
+      domString += '<h2 class="text-center p-4" id="foodTitle">FOODS</h2>';
+      domString += '<div class="d-flex flex-wrap">';
+      foods.forEach((food) => {
+        domString += foodComponent.foodDataCardInfo(food);
       });
+      domString += '</div>';
+      utils.printToDom('foodCards', domString);
     })
     .catch((err) => console.error('build all foods has failed you', err));
 };
--- src/javascripts/components/foods/foodComponent.js
+++ src/javascripts/components/foods/foodComponent.js
@@ -1,13 +1,21 @@
+import './food.scss';
+
 const foodDataCardInfo = (food) => {
   let domString = '';
-  domString += '<div class="card">';
+  domString += '<div class="col-4 d-flex align-items-stretch">';
+  domString += '<div id="allFoodCards" class="card">';
+  domString += `<h2 class="text-center">${food.type}</h2>`;
+  domString += `<img id="foodImages" class="card-img-top p-4" src="${food.imageUrl}" alt="${food.description}">`;
   domString += '<div class="card-body">';
-  domString += '<p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>';
-  domString += `<img class="card-img-top" src="${food.imageUrl}" alt="${food.description}">`;
+  domString += `<h3>${food.description}</h3>`;
+  domString += `<p>Price: $${food.price}</p>`;
+  domString += `<p>Where: ${food.location}</p>`;
+  domString += `<p>Currently Avaliable: ${food.isAvaliable}</p>`;
+  domString += '</div>';
   domString += '</div>';
   domString += '</div>';
 
   return domString;
 };
 
-export default { foodDataCardInfo }
+export default { foodDataCardInfo };
