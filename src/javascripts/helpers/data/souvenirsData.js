91fc2977364a56a6ce8aa2685f55d865f505b42c
Liza Star <lizadstar18@gmail.com>
2020-04-15 21:11:42 -0500

    added souvenirs read only view

--- src/javascripts/helpers/data/souvenirsData.js
+++ src/javascripts/helpers/data/souvenirsData.js
@@ -2,3 +2,21 @@ import axios from 'axios';
 import apiKeys from '../apiKeys.json';
 
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getSouvenirs = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/souvenirs.json`)
+    .then((response) => {
+      const allSouvenirs = response.data;
+      const souvenirs = [];
+      if (allSouvenirs) {
+        Object.keys(allSouvenirs).forEach((souvenirId) => {
+          allSouvenirs[souvenirId].id = souvenirId;
+          souvenirs.push(allSouvenirs[souvenirId]);
+        });
+      }
+      resolve(souvenirs);
+    })
+    .catch((err) => reject(err));
+});
+
+export default { getSouvenirs };
