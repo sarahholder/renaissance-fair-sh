3f6c5839e629af35fa8c7dd9df45dfb1408a6360
Chris Johnson <christopherjohnson1@gmail.com>
2020-04-17 14:28:53 -0400

    wip

---
+++ src/javascripts/helpers/data/staffData.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getStaff = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/staff.json`)
+    .then((response) => {
+      const allStaff = response.data;
+      const staff = [];
+      if (allStaff) {
+        Object.keys(allStaff).forEach((staffId) => {
+          allStaff[staffId].id = staffId;
+          staff.push(allStaff[staffId]);
+        });
+      }
+      resolve(staff);
+    })
+    .catch((err) => reject(err));
+});
+
+export default { getStaff };
