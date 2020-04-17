59b9807633b316df52acd6c5fa37e88504cfc3f4
Chris Johnson <christopherjohnson1@gmail.com>
2020-04-17 16:55:25 -0400

    staff cards print

--- src/javascripts/components/staff/staffCards.js
+++ src/javascripts/components/staff/staffCards.js
@@ -4,7 +4,7 @@ const buildStaffCards = (staff) => {
   let domString = '';
   domString += '<div class="col-4 mb-2">';
   domString += `<div class="card" id="${staff.id}">`;
-  domString += `<img src="${staff.imageUrl}" class="card-img-top" alt="Photo of ${staff.name}"`;
+  domString += `<img src="${staff.imageUrl}" class="card-img-top" alt="Photo of ${staff.name}">`;
   domString += '<div class="card-body">';
   domString += `<h5 class="card-title">${staff.name}</h5>`;
   domString += `<p class="card-text">Character Type: ${staff.characterType}</p>`;
