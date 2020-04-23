c0cee6f2687bf65355c1a551340ec10ef41121ad
Anca Simon <ancarsimon@gmail.com>
2020-04-23 14:15:38 -0500

    did single event view

--- src/javascripts/components/eventSingleView/eventSingleView.js
+++ src/javascripts/components/eventSingleView/eventSingleView.js
@@ -1,23 +1,52 @@
 import './eventSingleView.scss';
+import '../../../styles/main.scss';
 
 import eventData from '../../helpers/data/eventData';
 
 import utils from '../../helpers/utils';
+
+const closeSingleEvent = () => {
+  utils.printToDom('single-view-event', '');
+  $('#foodCards').removeClass('hide');
+  $('#souvenirs').removeClass('hide');
+  $('#staff-collection').removeClass('hide');
+  $('#shows').removeClass('hide');
+  $('#events').removeClass('hide');
+  $('#single-view-event').addClass('hide');
+};
 
 const viewSingleEvent = (eventId) => {
   eventData.getSingleEventWithDetails(eventId)
     .then((singleEvent) => {
       let domString = '';
       domString += '<div class="singleEventTitle">';
+      domString += '<div class="row">';
       domString += `<h2>${singleEvent.name}</h2>`;
-      domString += `<h4>${singleEvent.location}</h4>`;
-      domString += `<h4>${singleEvent.timeStart} - ${singleEvent.timeEnd}</h4>`;
+      domString += '<button id="closeSingleEvent" class="col-3 btn btn-default closeEventBtn ml-auto"><i class="fas fa-times"></i> Close</button>';
       domString += '</div>';
-      domString += '<div id="eventFoodSection">Food Details</div>';
-      domString += '<div id="eventSouvenirsSection">Souvenirs Details</div>';
-      domString += '<div id="eventStaffSection">Staff Details</div>';
-      domString += '<div id="eventShowsSection">Shows Details</div>';
+      domString += `<h5>${singleEvent.location}</h5>`;
+      domString += `<h5>${singleEvent.timeStart} - ${singleEvent.timeEnd}</h5>`;
+      domString += '</div>';
+      domString += '<div id="eventDetails">';
+      domString += '<div id="eventFoodSection" class="quad">';
+      domString += '<h4 class="eventSectionTitle">Food Details</h4>';
+      domString += '<p>DETAILS HERE!!!!</p>';
+      domString += '</div>';
+      domString += '<div id="eventSouvenirsSection" class="quad">';
+      domString += '<h4 class="eventSectionTitle">Souvenirs Details</h4>';
+      domString += '<p>DETAILS HERE!!!!</p>';
+      domString += '</div>';
+      domString += '<div id="eventStaffSection" class="quad">';
+      domString += '<h4 class="eventSectionTitle">Staff Details</h4>';
+      domString += '<p>DETAILS HERE!!!!</p>';
+      domString += '</div>';
+      domString += '<div id="eventShowsSection" class="quad">';
+      domString += '<h4 class="eventSectionTitle">Shows Details</h4>';
+      domString += '<p>DETAILS HERE!!!!</p>';
+      domString += '</div>';
+      domString += '</div>';
       utils.printToDom('single-view-event', domString);
+      $('body').on('click', '#closeSingleEvent', closeSingleEvent);
       $('#foodCards').addClass('hide');
       $('#souvenirs').addClass('hide');
       $('#staff-collection').addClass('hide');
