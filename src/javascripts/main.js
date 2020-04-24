28150671d9a5cfe97ed346a2b35beafe248f0e6e
Michele Rawlins <shellbop@att.net>
2020-04-24 13:08:35 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -5,7 +5,7 @@ import apiKeys from './helpers/apiKeys.json';
 import authData from './helpers/data/authData';
 import auth from './components/auth/auth';
 
-import shows from './components/shows/shows';
+// import shows from './components/shows/shows';
 import souvenirs from './components/souvenirs/souvenirs';
 // import staff from './components/staff/staff';
 
