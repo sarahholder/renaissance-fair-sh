4141853c179ac83525777fd559d611c1013233a7
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-04-30 12:24:59 -0500

    added id back

--- src/javascripts/components/eventSingleView/eventStaffDetails.js
+++ src/javascripts/components/eventSingleView/eventStaffDetails.js
@@ -39,9 +39,9 @@ const getEventStaffDetails = (singleEvent) => {
   domString += '</thead>';
   domString += '<tbody>';
   singleEvent.staff.forEach((staffMember) => {
-    domString += `<tr class="eventStaffMember staffRow" data-id="${staffMember.id}" data-parent="${staffMember.parentEventStaffId}" data-container="${staffMember.parentEventId}">`;
+    domString += `<tr class="eventStaffMember staffRow"id="${staffMember.parentEventId}" data-id="${staffMember.id}" data-parent="${staffMember.parentEventStaffId}" data-container="${staffMember.parentEventId}">`;
     domString += `<th scope="row" class="cell-width">${staffMember.name}</th>`;
-    domString += `<td class="cell-width">$${staffMember.pay}/hr.</td>`;
+    domString += `<td class="cell-width">$${staffMember.pay}</td>`;
     domString += `<td class="cell-width">${staffMember.parentQuantity}</td>`;
     getStaffTotals(singleEvent);
     domString += `<td class="cell-width">$${staffMember.rowTotal}</td>`;
