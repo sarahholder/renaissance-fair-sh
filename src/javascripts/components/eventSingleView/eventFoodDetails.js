86b2a6685a74c4ea7488911e8646ea07d0eb2418
Anca Simon <ancarsimon@gmail.com>
2020-05-03 01:00:11 -0500

    added filter for price range

--- src/javascripts/components/eventSingleView/eventFoodDetails.js
+++ src/javascripts/components/eventSingleView/eventFoodDetails.js
@@ -18,11 +18,33 @@ const getEventFoodDetails = (singleEvent) => {
   domString += '</thead>';
   domString += '<tbody>';
   singleEvent.food.forEach((foodItem) => {
-    domString += `<tr class="eventFoodItem foodRow" data-id="${foodItem.id}" data-parent="${foodItem.parentEventFoodId}" data-container="${foodItem.parentEventId}">`;
+    // if (`${foodItem.price}` < 101 && `${foodItem.price}` > 0) {
+    //   domString += '<div class="from0To100">';
+    // } else if (`${foodItem.price}` > 100 && `${foodItem.price}` < 201) {
+    //   domString += '<div class="from101To200">';
+    // } else if (`${foodItem.price}` > 200 && `${foodItem.price}` < 301) {
+    //   domString += '<div class="from201To300">';
+    // }
+    if (`${foodItem.price}` < 101 && `${foodItem.price}` > 0) {
+      domString += `<tr class="eventFoodItem foodRow from0To100" data-id="${foodItem.id}" data-parent="${foodItem.parentEventFoodId}" data-container="${foodItem.parentEventId}">`;
+    } else if (`${foodItem.price}` > 100 && `${foodItem.price}` < 201) {
+      domString += `<tr class="eventFoodItem foodRow from101To200" data-id="${foodItem.id}" data-parent="${foodItem.parentEventFoodId}" data-container="${foodItem.parentEventId}">`;
+    } else if (`${foodItem.price}` > 200 && `${foodItem.price}` < 301) {
+      domString += `<tr class="eventFoodItem foodRow from201To300" data-id="${foodItem.id}" data-parent="${foodItem.parentEventFoodId}" data-container="${foodItem.parentEventId}">`;
+    } else if (`${foodItem.price}` > 300 && `${foodItem.price}` < 401) {
+      domString += `<tr class="eventFoodItem foodRow from301To400" data-id="${foodItem.id}" data-parent="${foodItem.parentEventFoodId}" data-container="${foodItem.parentEventId}">`;
+    } else if (`${foodItem.price}` > 400 && `${foodItem.price}` < 501) {
+      domString += `<tr class="eventFoodItem foodRow from501To500" data-id="${foodItem.id}" data-parent="${foodItem.parentEventFoodId}" data-container="${foodItem.parentEventId}">`;
+    } else if (`${foodItem.price}` > 500) {
+      domString += `<tr class="eventFoodItem foodRow from501On" data-id="${foodItem.id}" data-parent="${foodItem.parentEventFoodId}" data-container="${foodItem.parentEventId}">`;
+    }
+    // Anca: INITIAL Table Row TAG WITHOUT ANY PRICE RANGE CLASSES BELOW
+    // domString += `<tr class="eventFoodItem foodRow" data-id="${foodItem.id}" data-parent="${foodItem.parentEventFoodId}" data-container="${foodItem.parentEventId}">`;
     domString += `<th scope="row" class="cell-width">${foodItem.type}</th>`;
     domString += `<td class="cell-width">$${foodItem.price}</td>`;
     domString += `<td class="cell-width">${foodItem.parentQuantity}</td>`;
     domString += `<td class="cell-width">$${foodItem.rowTotal}</td>`;
+    domString += '</div>';
     const user = firebase.auth().currentUser;
     if (user.uid === singleEvent.uid) {
       domString += `<td class="cell-width"><button id="deleteEventFoodBtn" class="btn btn-default deleteEventBtn deleteEventFoodBtn" data-id="${foodItem.parentEventFoodId}"><i class="far fa-trash-alt"></i></button></td>`;
